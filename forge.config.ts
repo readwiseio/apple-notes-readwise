9ea4ff238d62e019a05e9b107896a14e25cfa74a
Scott Carvalho <scottcarvalho71@gmail.com>
2024-11-10 11:33:07 -0500

    Add GitHub publish

--- forge.config.ts
+++ forge.config.ts
@@ -23,6 +23,19 @@ const config: ForgeConfig = {
   },
   rebuildConfig: {},
   makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: process.env.GITHUB_OWNER || '',
+          name: process.env.GITHUB_REPO || '',
+        },
+        draft: true,
+        prerelease: true,
+      },
+    },
+  ],
   plugins: [
     new VitePlugin({
       // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
