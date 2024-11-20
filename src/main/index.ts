82570a7866aee56380491273d1b366225ee82dd5
Scott Carvalho <scottcarvalho71@gmail.com>
2024-11-20 16:07:44 -0500

    Add automatic app updater #9, custom menu template, and make scripts

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,8 +1,11 @@
-import { app, BrowserWindow, ipcMain, shell } from 'electron'
+import { app, BrowserWindow, ipcMain, Menu, shell } from 'electron'
 import path from 'path'
 import { updateAppleNotesAccounts } from '@/lib/utils'
 import { getObsidianClientID, getUserAuthToken, ReadwiseSync } from '@/lib'
 import { store } from '@/lib/store'
+import { updateElectronApp } from 'update-electron-app'
+
+updateElectronApp()
 
 let mainWindow: BrowserWindow
 
@@ -29,13 +32,57 @@ const createWindow = () => {
     mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`))
   }
 
+  const isDev = !app.isPackaged
+
+  const customMenu = {
+    label: 'Help',
+    submenu: [
+      {
+        label: 'Documentation',
+        click: async () => {
+          shell.openExternal('https://github.com/Scarvy/apple-notes-readwise/wiki/User-Guide')
+        }
+      },
+      {
+        label: 'Report an Issue',
+        click: async () => {
+          shell.openExternal('https://github.com/Scarvy/apple-notes-readwise/issues')
+        }
+      },
+      {
+        label: 'Check latest release',
+        click: async () => {
+          shell.openExternal('https://github.com/Scarvy/apple-notes-readwise/releases')
+        }
+      },
+      {
+        label: 'Permission Issues?',
+        click: async () => {
+          shell.openExternal('https://scottsplace.notion.site/Apple-Notes-Readwise-Export-Fixing-permission-issues-14474debfabc805e8701f8534d1854a8?pvs=4')
+        }
+      },
+      {
+        label: 'Contact Developer',
+        click: async () => {
+          shell.openExternal('mailto:scottcarvalho71@gmail.com')
+        }
+      }
+    ]
+  }
+
+  const defaultMenu = Menu.getApplicationMenu()
+
+  const menuTemplate = [...(defaultMenu ? defaultMenu.items : []), customMenu]
+
+  const menu = Menu.buildFromTemplate(menuTemplate)
+  Menu.setApplicationMenu(menu)
+
   // check if the user is authenticated
   const tokenExsits = Boolean(store.get('token'))
   console.log('User is authenticated: ', tokenExsits)
   mainWindow.webContents.once('did-finish-load', () => {
     mainWindow.webContents.send('login-status', tokenExsits)
     if (tokenExsits) {
-
       // Configure scheduled sync
       const syncFrequency = store.get('frequency') || '0' // default to manual
       configureScheduledSync(syncFrequency)
@@ -46,12 +93,18 @@ const createWindow = () => {
       if (triggerOnLoad) {
         // if sync is already in progress, don't start another one
         if (Boolean(store.get('isSyncing'))) {
-          mainWindow.webContents.send('toast:show', { variant: 'default', message: 'Sync already in progress...'})
+          mainWindow.webContents.send('toast:show', {
+            variant: 'default',
+            message: 'Sync already in progress...'
+          })
           console.log('Sync already in progress')
           return
         }
 
-        mainWindow.webContents.send('toast:show', { variant: 'default', message: 'Initiating sync...'})
+        mainWindow.webContents.send('toast:show', {
+          variant: 'default',
+          message: 'Initiating sync...'
+        })
         const readwiseSync = new ReadwiseSync(mainWindow, store)
         readwiseSync.syncHighlights(undefined, true)
         console.log('Syncing highlights on load')
@@ -63,7 +116,10 @@ const createWindow = () => {
   mainWindow.webContents.setWindowOpenHandler(({ url }) => {
     shell.openExternal(url)
     return { action: 'deny' }
-  });
+  })
+
+  // Open the DevTools if the app is in development mode
+  isDev && mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
@@ -85,15 +141,17 @@ ipcMain.on('electron-store-set', async (_, key, value) => {
 })
 
 ipcMain.handle('sync-highlights', (_event, auto?: boolean) => {
-
   // if sync is already in progress, don't start another one
   if (Boolean(store.get('isSyncing'))) {
-    mainWindow.webContents.send('toast:show', { variant: 'default', message: 'Sync already in progress...'})
+    mainWindow.webContents.send('toast:show', {
+      variant: 'default',
+      message: 'Sync already in progress...'
+    })
     console.log('Sync already in progress')
     return
   }
 
-  mainWindow.webContents.send('toast:show', { variant: 'default', message: 'Initiating sync...'})
+  mainWindow.webContents.send('toast:show', { variant: 'default', message: 'Initiating sync...' })
 
   const readwiseSync = new ReadwiseSync(mainWindow, store)
   return readwiseSync.syncHighlights(undefined, auto)
@@ -135,7 +193,7 @@ ipcMain.handle('update-sync-frequency', async (_event, frequency: string) => {
 async function configureScheduledSync(frequency: string) {
   const minutes = parseInt(frequency)
   let milliseconds = minutes * 60 * 1000 // convert minutes to milliseconds
-  console.log("Settings interval to ", milliseconds)
+  console.log('Settings interval to ', milliseconds)
   if (!milliseconds) {
     // user set frequency to "Manual"
     return '0'
