d0731557571ae93a948e1a63fc47f32aea2a800a
Scott Carvalho <scottcarvalho71@gmail.com>
2025-01-08 14:37:32 -0500

    Update sql query to exclude folders 'marked for deletion'
    This will ensure we are looking in the correct folder when syncing and not a recently deleted folder with the same name

--- src/main/lib/apple-notes.ts
+++ src/main/lib/apple-notes.ts
@@ -1,13 +1,13 @@
 // @ts-nocheck
-import { BrowserWindow } from "electron";
-import protobufjs from "protobufjs";
-import path from "node:path";
-import os from "node:os";
-import fs from "node:fs";
-import { promises as fsPromises } from "fs";
-import { descriptor } from "@shared/descriptor";
-import zlib from "node:zlib";
-import { NOTE_FOLDER_PATH, NOTE_DB } from "@shared/constants";
+import { BrowserWindow } from 'electron'
+import protobufjs from 'protobufjs'
+import path from 'node:path'
+import os from 'node:os'
+import fs from 'node:fs'
+import { promises as fsPromises } from 'fs'
+import { descriptor } from '@shared/descriptor'
+import zlib from 'node:zlib'
+import { NOTE_FOLDER_PATH, NOTE_DB } from '@shared/constants'
 import {
   Note,
   ANNoteData,
@@ -15,92 +15,99 @@ import {
   NoteFolder,
   ANAttachment,
   ANAccount,
-  SQLiteTagSpawned,
-} from "@shared/models";
-import { NoteConverter } from "./convert-note";
+  SQLiteTagSpawned
+} from '@shared/models'
+import { NoteConverter } from './convert-note'
 // @ts-ignore
-import SQLiteTag from "./sqlite/index.js";
+import SQLiteTag from './sqlite/index.js'
 
-const { Root } = protobufjs;
+const { Root } = protobufjs
 
 // source: https://github.com/obsidianmd/obsidian-importer/blob/577036ad55fe79c92eeee6f961f8073da26622f5/src/filesystem.ts#L228
 export function splitext(name: string) {
-  let dotIndex = name.lastIndexOf(".");
-  let basename = name;
-  let extension = "";
+  let dotIndex = name.lastIndexOf('.')
+  let basename = name
+  let extension = ''
 
   if (dotIndex > 0) {
-    basename = name.substring(0, dotIndex);
-    extension = name.substring(dotIndex + 1).toLowerCase();
+    basename = name.substring(0, dotIndex)
+    extension = name.substring(dotIndex + 1).toLowerCase()
   }
 
-  return [basename, extension];
+  return [basename, extension]
 }
 
 // Source: https://github.com/obsidianmd/obsidian-importer/blob/577036ad55fe79c92eeee6f961f8073da26622f5/src/formats/apple-notes.ts#L18
 export class AppleNotesExtractor {
-  window: BrowserWindow;
+  window: BrowserWindow
 
-  database: SQLiteTagSpawned | null = null;
-  protobufRoot: Root;
-  useHTMLformat: boolean;
+  database: SQLiteTagSpawned | null = null
+  protobufRoot: Root
+  useHTMLformat: boolean
 
-  keys: Record<string, number> = {};
-  owners: Record<number, number> = {};
+  keys: Record<string, number> = {}
+  owners: Record<number, number> = {}
 
-  accountID = 0;
-  account: ANAccount = { name: "", uuid: "", path: "" };
-  folder: NoteFolder = { z_pk: 0, ztitle: "" };
+  accountID = 0
+  account: ANAccount = { name: '', uuid: '', path: '' }
+  folder: NoteFolder = { z_pk: 0, ztitle: '' }
 
-  noteCount = 0;
-  parsedNotes = 0;
-  omitFirstLine = false;
-  isICAccount = true; // Assume it's an iCloud account by default, until proven otherwise
+  noteCount = 0
+  parsedNotes = 0
+  omitFirstLine = false
+  isICAccount = true // Assume it's an iCloud account by default, until proven otherwise
 
   constructor(mainWindow: BrowserWindow, omitFirstLine = false, useHTMLformat = true) {
-    this.protobufRoot = Root.fromJSON(descriptor);
-    this.window = mainWindow;
-    this.omitFirstLine = omitFirstLine;
+    this.protobufRoot = Root.fromJSON(descriptor)
+    this.window = mainWindow
+    this.omitFirstLine = omitFirstLine
     this.useHTMLformat = useHTMLformat
   }
 
   async init(folder: string, account: string): Promise<void> {
-    console.log("MAIN: Initializing Apple Notes Extractor...");
-    console.log(`MAIN: Getting notes database from ${folder}...`);
-    this.database = (await this.getNotesDatabase()) as SQLiteTagSpawned;
-    if (!this.database) return;
-    console.log("MAIN: Database loaded...");
+    console.log('MAIN: Initializing Apple Notes Extractor...')
+    console.log(`MAIN: Getting notes database from ${folder}...`)
+    this.database = (await this.getNotesDatabase()) as SQLiteTagSpawned
+    if (!this.database) return
+    console.log('MAIN: Database loaded...')
 
-    console.log("MAIN: Getting primary keys...");
+    console.log('MAIN: Getting primary keys...')
     this.keys = Object.fromEntries(
-      (await this.database.all`SELECT z_ent, z_name FROM z_primarykey`).map(
-        (k) => [k.Z_NAME, k.Z_ENT]
-      )
-    );
+      (await this.database.all`SELECT z_ent, z_name FROM z_primarykey`).map((k) => [
+        k.Z_NAME,
+        k.Z_ENT
+      ])
+    )
 
-    console.log("MAIN: Getting note account...");
+    console.log('MAIN: Getting note account...')
     const noteAccount = await this.database.all`
-      SELECT Z_PK as z_pk FROM ziccloudsyncingobject WHERE z_ent = ${this.keys.ICAccount}`;
-    console.log("MAIN: noteAccount: ", noteAccount);
+      SELECT Z_PK as z_pk FROM ziccloudsyncingobject WHERE z_ent = ${this.keys.ICAccount}`
+    console.log('MAIN: noteAccount: ', noteAccount)
 
-    console.log("MAIN: Getting note folder...");
+    console.log('MAIN: Getting note folder...')
     this.folder = (
       await this.database
-        .all`SELECT Z_PK as z_pk, ZTITLE2 as ztitle2 FROM ziccloudsyncingobject WHERE z_ent = ${this.keys.ICFolder} AND ztitle2 = ${folder}`
-    )[0];
-    console.log("MAIN: Folder: ", this.folder);
-    await this.resolveAccount(noteAccount[0].z_pk);
+        .all`
+        SELECT 
+            Z_PK as z_pk, ZTITLE2 as ztitle2 
+        FROM ziccloudsyncingobject 
+        WHERE z_ent = ${this.keys.ICFolder} 
+        AND zmarkedfordeletion = 0 
+        AND ztitle2 = ${folder}
+      `)[0]
+    console.log('MAIN: Folder: ', this.folder)
+    await this.resolveAccount(noteAccount[0].z_pk)
 
     // determine the account we are working with
     this.isICAccount = await this.resolveAccountType(account)
   }
 
   async getAccountType(): boolean {
-    return this.isICAccount;
+    return this.isICAccount
   }
 
   async getNotesDatabase(): Promise<SQLiteTagSpawned | null> {
-    console.log("MAIN: Getting notes database...");
+    console.log('MAIN: Getting notes database...')
     const dataPath = path.join(os.homedir(), NOTE_FOLDER_PATH)
     if (!fs.existsSync(dataPath)) {
       console.error('MAIN: Apple Notes data path not found...')
@@ -108,30 +115,30 @@ export class AppleNotesExtractor {
     }
 
     const originalDB = path.join(dataPath, NOTE_DB)
-    const cloneDB = path.join(os.tmpdir(), NOTE_DB);
+    const cloneDB = path.join(os.tmpdir(), NOTE_DB)
 
     // copy the database to a temporary location
     await fsPromises.copyFile(originalDB, cloneDB)
     await fsPromises.copyFile(originalDB + '-shm', cloneDB + '-shm')
     await fsPromises.copyFile(originalDB + '-wal', cloneDB + '-wal')
 
-    return new SQLiteTag(cloneDB, { readonly: true, persistent: true });
+    return new SQLiteTag(cloneDB, { readonly: true, persistent: true })
   }
 
   close() {
-    console.log("MAIN: Closing Apple Notes Extractor...");
+    console.log('MAIN: Closing Apple Notes Extractor...')
     if (this.database) {
-      this.database.close();
-      console.log("MAIN: Database closed...");
+      this.database.close()
+      console.log('MAIN: Database closed...')
     }
   }
 
   async extractNoteHTML(name: string): Promise<string | void> {
     if (!this.database) {
-      console.error("MAIN: Database not found...");
-      return;
+      console.error('MAIN: Database not found...')
+      return
     }
-    console.log("MAIN: Extracting note: ", name);
+    console.log('MAIN: Extracting note: ', name)
 
     const notes = await this.database.get`
     SELECT
@@ -142,22 +149,28 @@ export class AppleNotesExtractor {
           AND ztitle1 IS NOT NULL
           AND zfolder = ${this.folder.z_pk}
           AND zfolder NOT IN (1)
-    `;
-    console.log("MAIN: Notes: ", notes);
+    `
+
+    // if no notes are found, return null and handle it in the main process as a failure and keep going
+    if (!notes) {
+      console.error('MAIN: Note not found: ', name)
+      return
+    }
+    console.log('MAIN: Note ', notes)
 
     // decode the protobuf
-    const html = await this.resolveNote(notes.z_pk);
-    console.log("HTML: ", html);
+    const html = await this.resolveNote(notes.z_pk)
+    console.log('HTML: ', html)
 
-    return html;
+    return html
   }
 
   async resolveNote(id: number): Promise<string | void> {
     if (!this.database) {
-      console.error("Database not found...");
-      return;
+      console.error('Database not found...')
+      return
     }
-    console.log("Resolving note ID: ", id);
+    console.log('Resolving note ID: ', id)
     const row = (await this.database.get`SELECT
             nd.z_pk, hex(nd.zdata) as zhexdata, zcso.ztitle1 as ztitle1, zfolder as zfolder,
             zcreationdate1 as zcreationdate1, zcreationdate2 as zcreationdate2, zcreationdate3 as zcreationdate3, 
@@ -170,94 +183,84 @@ export class AppleNotesExtractor {
             ) AS zcso
         WHERE
             zcso.z_pk = nd.znote
-            AND zcso.z_pk = ${id}`) as ANNoteData;
+            AND zcso.z_pk = ${id}`) as ANNoteData
 
     if (!row) {
-      console.error("Note not found: ", id);
-      return;
+      console.error('Note not found: ', id)
+      return
     }
 
     // Write the html to a file for debugging purposes... for now
-    const title = `${row.ztitle1}.html`;
-    const file = path.join(os.homedir(), "Documents", "Readwise", title);
-    console.log(`Resolving note: ${title}`);
+    const title = `${row.ztitle1}.html`
+    const file = path.join(os.homedir(), 'Documents', 'Readwise', title)
+    console.log(`Resolving note: ${title}`)
 
     // Decode the protobuf into HTML
-    const converter = this.decodeData(row.zhexdata, NoteConverter);
-    console.log("Successfully decoded note data...");
-    const html = await converter.format();
-    console.log("Successfully formatted note data to html...");
-    console.log("MAIN: Final HTML: ", html);
+    const converter = this.decodeData(row.zhexdata, NoteConverter)
+    console.log('Successfully decoded note data...')
+    const html = await converter.format()
+    console.log('Successfully formatted note data to html...')
+    console.log('MAIN: Final HTML: ', html)
 
     // Write the file
-    fs.writeFileSync(file, html, "utf8");
-    return html;
+    fs.writeFileSync(file, html, 'utf8')
+    return html
   }
 
   async resolveAccountType(name: string): Promise<boolean> {
     if (!this.database) {
-      console.error("Database not found...");
-      return;
+      console.error('Database not found...')
+      return
     }
-    console.log("MAIN: checking account type for", name);
+    console.log('MAIN: checking account type for', name)
     const account = await this.database.get`
       SELECT ZNAME as zname, ZIDENTIFIER as zidentifier FROM ziccloudsyncingobject
       WHERE z_ent = ${this.keys.ICAccount} AND zname = ${name}
-    `;
-    console.log("Account: ", account);
-    return true ? account !== undefined : false;
+    `
+    console.log('Account: ', account)
+    return true ? account !== undefined : false
   }
 
   async resolveAccount(id: number): Promise<void> {
     if (!this.database) {
-      console.error("Database not found...");
-      return;
+      console.error('Database not found...')
+      return
     }
-    console.log("Resolving account ID: ", id);
+    console.log('Resolving account ID: ', id)
     const account = await this.database.get`
 			SELECT ZNAME as zname, ZIDENTIFIER as zidentifier FROM ziccloudsyncingobject
 			WHERE z_ent = ${this.keys.ICAccount} AND z_pk = ${id}
-		`;
+		`
 
     this.account = {
       name: account.zname,
       uuid: account.zidentifier,
-      path: path.join(
-        os.homedir(),
-        NOTE_FOLDER_PATH,
-        "Accounts",
-        account.zidentifier
-      ),
-    };
-    console.log("Account: ", this.account);
+      path: path.join(os.homedir(), NOTE_FOLDER_PATH, 'Accounts', account.zidentifier)
+    }
+    console.log('Account: ', this.account)
   }
 
   decodeData(hexdata: string, converterType: any) {
     // TODO: Implement the converterType
-    console.log("Decoding note data...");
-    const unzipped = zlib.gunzipSync(Buffer.from(hexdata, "hex"));
-    const decoded = this.protobufRoot
-      .lookupType("ciofecaforensics.Document")
-      .decode(unzipped);
-    return new converterType(this, decoded, this.useHTMLformat);
+    console.log('Decoding note data...')
+    const unzipped = zlib.gunzipSync(Buffer.from(hexdata, 'hex'))
+    const decoded = this.protobufRoot.lookupType('ciofecaforensics.Document').decode(unzipped)
+    return new converterType(this, decoded, this.useHTMLformat)
   }
 
-  async resolveAttachment(
-    id: number,
-    uti: ANAttachment | string
-  ): Promise<any | null> {
+  async resolveAttachment(id: number, uti: ANAttachment | string): Promise<any | null> {
     if (!this.database) {
-      console.error("Database not found...");
-      return null;
+      console.error('Database not found...')
+      return null
     }
 
-    console.log("Resolving attachment ID: ", id);
-    let sourcePath, outName, outExt, row, file;
+    console.log('Resolving attachment ID: ', id)
+    let sourcePath, outName, outExt, row, file
 
     switch (uti) {
       case ANAttachment.ModifiedScan:
         // A PDF only seems to be generated when you modify the scan :(
-        console.log("Modified Scan");
+        console.log('Modified Scan')
         row = await this.database.get`
   				SELECT
   					zidentifier, zfallbackpdfgeneration, zcreationdate, zmodificationdate, znote
@@ -266,20 +269,20 @@ export class AppleNotesExtractor {
   				WHERE
   					z_ent = ${this.keys.ICAttachment}
   					AND z_pk = ${id}
-  			`;
+  			`
 
         sourcePath = path.join(
-          "FallbackPDFs",
+          'FallbackPDFs',
           row.ZIDENTIFIER,
-          row.ZFALLBACKPDFGENERATION || "",
-          "FallbackPDF.pdf"
-        );
-        outName = "Scan";
-        outExt = "pdf";
-        break;
+          row.ZFALLBACKPDFGENERATION || '',
+          'FallbackPDF.pdf'
+        )
+        outName = 'Scan'
+        outExt = 'pdf'
+        break
 
       case ANAttachment.Scan:
-        console.log("Scan");
+        console.log('Scan')
         row = await this.database.get`
   				SELECT
   					zidentifier, zsizeheight, zsizewidth, zcreationdate, zmodificationdate, znote
@@ -287,18 +290,18 @@ export class AppleNotesExtractor {
   				WHERE
   					z_ent = ${this.keys.ICAttachment}
   					AND z_pk = ${id}
-  			`;
+  			`
 
         sourcePath = path.join(
-          "Previews",
+          'Previews',
           `${row.ZIDENTIFIER}-1-${row.ZSIZEWIDTH}x${row.ZSIZEHEIGHT}-0.jpeg`
-        );
-        outName = "Scan Page";
-        outExt = "jpg";
-        break;
+        )
+        outName = 'Scan Page'
+        outExt = 'jpg'
+        break
 
       case ANAttachment.Drawing:
-        console.log("Drawing");
+        console.log('Drawing')
         row = await this.database.get`
   				SELECT
   					zidentifier, zfallbackimagegeneration, zcreationdate, zmodificationdate,
@@ -308,26 +311,26 @@ export class AppleNotesExtractor {
   				WHERE
   					z_ent = ${this.keys.ICAttachment}
   					AND z_pk = ${id}
-  			`;
+  			`
 
         if (row.ZFALLBACKIMAGEGENERATION) {
           // macOS 14/iOS 17 and above
           sourcePath = path.join(
-            "FallbackImages",
+            'FallbackImages',
             row.ZIDENTIFIER,
             row.ZFALLBACKIMAGEGENERATION,
-            "FallbackImage.png"
-          );
+            'FallbackImage.png'
+          )
         } else {
-          sourcePath = path.join("FallbackImages", `${row.ZIDENTIFIER}.jpg`);
+          sourcePath = path.join('FallbackImages', `${row.ZIDENTIFIER}.jpg`)
         }
 
-        outName = "Drawing";
-        outExt = "png";
-        break;
+        outName = 'Drawing'
+        outExt = 'png'
+        break
 
       default:
-        console.log("Default");
+        console.log('Default')
         row = await this.database.get`
   				SELECT
   					a.zidentifier, a.zfilename,
@@ -339,36 +342,26 @@ export class AppleNotesExtractor {
   					a.z_ent = ${this.keys.ICMedia}
   					AND a.z_pk = ${id}
   					AND a.z_pk = b.zmedia
-  			`;
+  			`
 
-        sourcePath = path.join(
-          "Media",
-          row.ZIDENTIFIER,
-          row.ZGENERATION1 || "",
-          row.ZFILENAME
-        );
-        [outName, outExt] = splitext(row.ZFILENAME);
-        break;
+        sourcePath = path.join('Media', row.ZIDENTIFIER, row.ZGENERATION1 || '', row.ZFILENAME)
+        ;[outName, outExt] = splitext(row.ZFILENAME)
+        break
     }
 
-    console.log("Source Path: ", sourcePath);
-    console.log("Out Name: ", outName);
-    console.log("Out Ext: ", outExt);
-    console.log("Row: ", row);
+    console.log('Source Path: ', sourcePath)
+    console.log('Out Name: ', outName)
+    console.log('Out Ext: ', outExt)
+    console.log('Row: ', row)
 
-    console.log("Account: ", this.account);
+    console.log('Account: ', this.account)
 
     try {
-      const binary = await this.getAttachmentSource(this.account, sourcePath);
-      const attachmentPath = path.join(
-        os.homedir(),
-        "Documents",
-        "Readwise",
-        "Attachments"
-      );
+      const binary = await this.getAttachmentSource(this.account, sourcePath)
+      const attachmentPath = path.join(os.homedir(), 'Documents', 'Readwise', 'Attachments')
 
       if (!fs.existsSync(attachmentPath)) {
-        fs.mkdirSync(attachmentPath, { recursive: true });
+        fs.mkdirSync(attachmentPath, { recursive: true })
       }
 
       // file = await this.vault.createBinary(
@@ -376,36 +369,27 @@ export class AppleNotesExtractor {
       // 	{ ctime: this.decodeTime(row.ZCREATIONDATE), mtime: this.decodeTime(row.ZMODIFICATIONDATE) }
       // );
       // create a binary file
-      fs.writeFileSync(
-        path.join(attachmentPath, `${outName}.${outExt}`),
-        binary,
-        "utf8"
-      );
+      fs.writeFileSync(path.join(attachmentPath, `${outName}.${outExt}`), binary, 'utf8')
     } catch (e) {
       // this.ctx.reportFailed(sourcePath);
-      console.error(e);
-      return null;
+      console.error(e)
+      return null
     }
 
     // this.resolvedFiles[id] = file;
     // this.ctx.reportAttachmentSuccess(this.resolvedFiles[id].path);
 
     // the path to the original attachment file
-    const attachmentFilePath = path.join(this.account.path, sourcePath);
-    console.log("File: ", attachmentFilePath);
-    return attachmentFilePath;
+    const attachmentFilePath = path.join(this.account.path, sourcePath)
+    console.log('File: ', attachmentFilePath)
+    return attachmentFilePath
   }
 
-  async getAttachmentSource(
-    account: ANAccount,
-    sourcePath: string
-  ): Promise<Buffer> {
+  async getAttachmentSource(account: ANAccount, sourcePath: string): Promise<Buffer> {
     try {
-      return await fsPromises.readFile(path.join(account.path, sourcePath));
+      return await fsPromises.readFile(path.join(account.path, sourcePath))
     } catch (e) {
-      return await fsPromises.readFile(
-        path.join(os.homedir(), NOTE_FOLDER_PATH, sourcePath)
-      );
+      return await fsPromises.readFile(path.join(os.homedir(), NOTE_FOLDER_PATH, sourcePath))
     }
   }
 }
