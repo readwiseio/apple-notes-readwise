a581857746ebc8dd42a901623e230feccb75215e
Scott Carvalho <scottcarvalho71@gmail.com>
2025-01-15 10:50:56 -0500

    Update baseURL to production and add debug option for local testing

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,4 +1,6 @@
-export const baseURL = "https://local.readwise.io:8000";
+export const baseURL = "https://readwise.io";
+// DEBUG ONLY
+// export const baseURL = "https://local.readwise.io:8000";
 
 export const NOTE_FOLDER_PATH = 'Library/Group Containers/group.com.apple.notes';
 export const NOTE_DB = 'NoteStore.sqlite';
